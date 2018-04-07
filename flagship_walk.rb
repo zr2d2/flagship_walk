89a0d5a44e25b63874a534adcd27c9dbcc5c6fe5
Zach Rowe <zrowe007@gmail.com>
2018-04-07 14:51:32 -0400

    add in code to calculate route weight

---
+++ flagship_walk.rb
@@ -0,0 +1,74 @@
+require 'sinatra'
+require 'httparty'
+
+helpers do
+  def get_obsticles(first, last)
+    params = {
+      query: {
+        "lat1" => first[:lat],
+        "lng1" => first[:lng],
+        "lat2" => last[:lat],
+        "lng2" => last[:lng]
+      }
+    }
+
+    api_url = 'http://sidewalk.umiacs.umd.edu/v1/access/features'
+
+    response = HTTParty.get api_url, params
+
+    json = JSON.parse response.body, symbolize_names: true
+
+    return json[:features]
+  end
+end
+
+get '/directions' do
+
+  key = ENV['GMAPS_KEY']
+
+  params = {
+    query: {
+      origin: '800 Lincoln Cir NE, Washington, DC 20002',
+      destination: '200 Florida Ave NE, Washington, DC 20002',
+      mode: 'walking',
+      alternatives: 'true',
+      key: key,
+      avoid: 'indoor'
+    }
+  }
+
+  api_url = 'https://maps.googleapis.com/maps/api/directions/json'
+
+  response = HTTParty.get(api_url, params)
+
+  json = JSON.parse response.body, symbolize_names: true
+
+  routes = Hash.new
+
+  json[:routes].each_with_index do |route, i|
+    features = Hash.new 0
+
+    route[:legs].each do |leg|
+      leg[:steps].each do |step|
+        step_start = step[:start_location]
+        step_end   = step[:end_location]
+
+        obstacles = get_obsticles(step_start, step_end)
+
+        obstacles.each do |obstacle|
+          label = obstacle[:properties][:label_type]
+
+          features[label] += 1
+        end
+      end
+    end
+
+    routes[i] = features["NoCurbRamp"] + features["Obstacle"] + features["SurfaceProblem"]
+  end
+
+  best_route_id = routes.to_a.sort{ |x, y| x.last <=> y.last }.first.first
+
+  best_route_text = json[:routes][best_route_id][:summary]
+
+  return "You're best to use #{best_route_text}"
+end
